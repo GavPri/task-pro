2c9fba62a9e5849d0461a2da2dc05edcb02fdc71
GavPri <gavinprior5@gmail.com>
2023-10-11 13:53:32 +0000

    initial deployment commit 1

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["SECRET_KEY"] = "SophMar13"
+os.environ["DATABASE_URL"] = "postgres://eaamxqow:esgOs8DsuhjHnNATmMBCCgjga8yzE-Mg@trumpet.db.elephantsql.com/eaamxqow"
+os.environ["CLOUDINARY_URL"] = "cloudinary://491583141319299:yYghEk3_HXIa18PaPAHrX6crby8@dugzh01xj"
